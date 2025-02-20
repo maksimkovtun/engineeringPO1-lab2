db84391e1b57f461b4f111a73be17ac179879f9b
maksimkovtun <makskovtun2002@mail.ru>
2025-02-20 18:59:28 +0700

    final

--- src/main/java/org/project/JsonSerializer.java
+++ src/main/java/org/project/JsonSerializer.java
@@ -1,7 +1,6 @@
 package org.project;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -9,6 +8,7 @@ import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Retention(RetentionPolicy.RUNTIME)
 @interface JsonField {
@@ -16,63 +16,47 @@ import java.util.Map;
 }
 
 public class JsonSerializer {
-    public static String serialize(Object obj) throws IllegalAccessException, JSONException {
+    public static JSONObject serializeObject(Object obj, Map<Object, Integer> objectMap, AtomicInteger idGenerator) throws Exception {
         if (obj == null) {
-            return "null";
+            return null;
         }
-        Class<?> clazz = obj.getClass();
-        JSONObject jsonObject = new JSONObject();
-        Map<Object, Integer> objectMap = new HashMap<>(); 
-        serializeObject(obj, jsonObject, objectMap);
-        return jsonObject.toString();
-    }
-    private static void serializeObject(Object obj, JSONObject jsonObject, Map<Object, Integer> objectMap) throws IllegalAccessException, JSONException {
-        if (obj == null) {
-            return;
-        }
-        Class<?> clazz = obj.getClass();
         if (objectMap.containsKey(obj)) {
-            jsonObject.put("$ref", objectMap.get(obj));
-            return;
-        } else {
-            objectMap.put(obj, objectMap.size());
-            jsonObject.put("$id", objectMap.size() - 1);
+            JSONObject refObject = new JSONObject();
+            refObject.put("$ref", objectMap.get(obj));
+            return refObject;
         }
+        JSONObject jsonObject = new JSONObject();
+        int id = idGenerator.getAndIncrement();
+        objectMap.put(obj, id);
+        jsonObject.put("$id", id);
+        Class<?> clazz = obj.getClass();
         for (Field field : clazz.getDeclaredFields()) {
             field.setAccessible(true);
             if (field.isAnnotationPresent(JsonField.class)) {
                 JsonField annotation = field.getAnnotation(JsonField.class);
                 String key = annotation.name().isEmpty() ? field.getName() : annotation.name();
                 Object value = field.get(obj);
-
                 if (value == null) {
                     jsonObject.put(key, JSONObject.NULL);
-                } else if (value.getClass().isArray()) {
+                } else if (field.getType().isArray()) {
                     JSONArray jsonArray = new JSONArray();
-                    for (int i = 0; i < Array.getLength(value); i++) {
+                    int length = Array.getLength(value);
+                    for (int i = 0; i < length; i++) {
                         Object element = Array.get(value, i);
-                        JSONObject elementJson = new JSONObject();
-                        serializeObject(element, elementJson, objectMap);
-                        jsonArray.put(elementJson);
+                        jsonArray.put(serializeObject(element, objectMap, idGenerator));
                     }
                     jsonObject.put(key, jsonArray);
-                } else if (value.getClass().isPrimitive() || value instanceof String || value instanceof Number || value instanceof Boolean) {
-                    jsonObject.put(key, value.toString());
+                } else if (field.getType().isPrimitive() || value instanceof String) {
+                    jsonObject.put(key, value);
                 } else {
-                    JSONObject nestedObject = new JSONObject();
-                    serializeObject(value, nestedObject, objectMap);
-                    jsonObject.put(key, nestedObject);
+                    jsonObject.put(key, serializeObject(value, objectMap, idGenerator));
                 }
             }
         }
+        return jsonObject;
     }
-    public static <T> T deserialize(String json, Class<T> clazz) throws Exception {
-        JSONObject jsonObject = new JSONObject(json);
-        Map<Integer, Object> objectMap = new HashMap<>();
-        return deserializeObject(jsonObject, clazz, objectMap);
-    }
-    private static <T> T deserializeObject(JSONObject jsonObject, Class<T> clazz, Map<Integer, Object> objectMap) throws Exception {
-        if (jsonObject == null || jsonObject.has("$ref")) {
+    public static <T> T deserializeObject(JSONObject jsonObject, Class<T> clazz, Map<Integer, Object> objectMap) throws Exception {
+        if (jsonObject.has("$ref")) {
             int refId = jsonObject.getInt("$ref");
             return (T) objectMap.get(refId);
         }
@@ -91,33 +75,15 @@ public class JsonSerializer {
                         JSONArray jsonArray = jsonObject.getJSONArray(key);
                         Object array = Array.newInstance(field.getType().getComponentType(), jsonArray.length());
                         for (int i = 0; i < jsonArray.length(); i++) {
-                            JSONObject elementJson = jsonArray.optJSONObject(i);
-                            if (elementJson != null) {
+                            if (jsonArray.get(i) instanceof JSONObject) {
+                                JSONObject elementJson = jsonArray.getJSONObject(i);
                                 Object element = deserializeObject(elementJson, field.getType().getComponentType(), objectMap);
                                 Array.set(array, i, element);
                             }
                         }
                         field.set(instance, array);
-                    } else if (field.getType().isPrimitive()) {
-                        if (field.getType().equals(int.class)) {
-                            field.set(instance, jsonObject.getInt(key));
-                        } else if (field.getType().equals(double.class)) {
-                            field.set(instance, jsonObject.getDouble(key));
-                        } else if (field.getType().equals(boolean.class)) {
-                            field.set(instance, jsonObject.getBoolean(key));
-                        } else if (field.getType().equals(long.class)) {
-                            field.set(instance, jsonObject.getLong(key));
-                        } else if (field.getType().equals(float.class)) {
-                            field.set(instance, (float) jsonObject.getDouble(key));
-                        } else if (field.getType().equals(short.class)) {
-                            field.set(instance, (short) jsonObject.getInt(key));
-                        } else if (field.getType().equals(byte.class)) {
-                            field.set(instance, (byte) jsonObject.getInt(key));
-                        } else {
-                            throw new IllegalArgumentException("Unsupported primitive type: " + field.getType());
-                        }
-                    } else if (field.getType().equals(String.class)) {
-                        field.set(instance, jsonObject.getString(key));
+                    } else if (field.getType().isPrimitive() || field.getType().equals(String.class)) {
+                        field.set(instance, jsonObject.get(key));
                     } else {
                         JSONObject nestedObject = jsonObject.getJSONObject(key);
                         Object nestedValue = deserializeObject(nestedObject, field.getType(), objectMap);
@@ -131,31 +97,41 @@ public class JsonSerializer {
 
         return instance;
     }
-    public static class TestClass {
+    public static class Person {
         @JsonField
         private String name;
-        @JsonField(name = "age")
+        @JsonField
         private int age;
         @JsonField
-        private TestClass[] friends;
-        public TestClass(String name, int age, TestClass[] friends) {
+        private Person[] friends;
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+        public Person(String name, int age, Person[] friends) {
             this.name = name;
             this.age = age;
             this.friends = friends;
         }
-        public TestClass() {}
+        public Person() {}
         @Override
         public String toString() {
-            return "TestClass{name='" + name + '\'' + ", age=" + age + '}';
+            return "Person{name='" + name + "', age=" + age + '}';
         }
     }
+
     public static void main(String[] args) throws Exception {
-        TestClass friend1 = new TestClass("Alice", 25, null);
-        TestClass friend2 = new TestClass("Bob", 30, null);
-        TestClass person = new TestClass("Charlie", 35, new TestClass[]{friend1, friend2});
-        String json = serialize(person);
-        System.out.println("Serialized JSON: " + json);
-        TestClass deserializedPerson = deserialize(json, TestClass.class);
-        System.out.println("Deserialized Object: " + deserializedPerson);
+        Person friend1 = new Person("Alice", 25);
+        Person friend2 = new Person("Bob", 30);
+        Person person = new Person("John", 20, new Person[]{friend1, friend2, friend1});
+        Map<Object, Integer> objectMap = new HashMap<>();
+        AtomicInteger idGenerator = new AtomicInteger(1);
+        JSONObject serializedPerson = serializeObject(person, objectMap, idGenerator);
+        System.out.println("Сериализованный JSON:");
+        System.out.println(serializedPerson.toString(4));
+        Map<Integer, Object> objectMapDeserialization = new HashMap<>();
+        Person deserializedPerson = deserializeObject(serializedPerson, Person.class, objectMapDeserialization);
+        System.out.println("\nДесериализованный объект:");
+        System.out.println(deserializedPerson);
     }
 }
